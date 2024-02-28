1fc9a5595e5c1ce4b050548d96acb3fb947cc116
Peter <10252129@stud.op.edu.ua>
2024-02-28 18:12:11 +0200

    Practice&challenges

--- 04-Data-Structures-Operators/challenge.js
+++ 04-Data-Structures-Operators/challenge.js
@@ -45,6 +45,7 @@ const game = {
   },
 };
 
+//////////////// Challenge #1
 // 1
 const [players1, players2] = game.players;
 console.log(players1);
@@ -73,3 +74,34 @@ game.printGoals(...game.scored);
 // 7
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
+
+//////////////// Challenge #2
+// 1
+for (const [index, player] of game.scored.entries()) {
+  console.log(`Goal ${index + 1}: ${player}`);
+}
+
+// for (let i = 0; i < game.scored.length; i++) {
+//   console.log(`Goal ${i + 1}: ${game.scored[i]}`);
+// }
+
+// 2
+let averageOdd = 0;
+const oddsValues = Object.values(game.odds);
+for (const odd of oddsValues) {
+  averageOdd += odd;
+}
+averageOdd /= oddsValues.length;
+console.log(averageOdd);
+
+// 3
+const oddsEntries = Object.entries(game.odds);
+for (const [team, odd] of oddsEntries) {
+  let teamStr;
+  if (team === 'x') {
+    teamStr = 'draw';
+  } else {
+    teamStr = `victory ${game[team]}`;
+  }
+  console.log(`Odd of ${teamStr} is ${odd}`);
+}
