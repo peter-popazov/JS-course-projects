4025805d678cb2e560a9b04a0cc16e3f7ee295fd
Peter <popazovpeta@gmail.com>
2024-03-19 19:13:04 +0200

    Challenge #3

--- 04-Data-Structures-Operators/challenge.js
+++ 04-Data-Structures-Operators/challenge.js
@@ -105,3 +105,44 @@ for (const [team, odd] of oddsEntries) {
   }
   console.log(`Odd of ${teamStr} is ${odd}`);
 }
+
+//////////////// Challenge #3
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+const events = new Set(gameEvents.values());
+console.log(events);
+
+//   2. After the game has finished, is was found that the yellow card from minute 64
+//   was unfair. So remove this event from the game events log.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//   3. Compute and log the following string to the console: "An event happened, on
+//   average, every 9 minutes" (keep in mind that a game has 90 minutes)
+const time = [...gameEvents.keys()].pop();
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+//   4. Loop over 'gameEvents' and log each element to the console, marking
+//   whether it's in the first half or second half (after 45 min) of the game, like this:
+//   [FIRST HALF] 17: ⚽ GOAL
+for (const [time, event] of gameEvents) {
+  if (time <= 45) {
+    console.log(`[FIRST HALF] ${time}: ${event}`);
+  } else if (time > 45) {
+    console.log(`[SECOND HALF] ${time}: ${event}`);
+  }
+}
